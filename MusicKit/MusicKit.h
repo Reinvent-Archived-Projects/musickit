1ebe4cd69c62098c5fb326bfd6aebf25b36d68a5
Alejandro Isaza <aisaza@venturemedia.com>
2014-11-03 13:03:26 -0800

    Initial Commit

---
+++ MusicKit/MusicKit.h
@@ -0,0 +1,19 @@
+//
+//  MusicKit.h
+//  MusicKit
+//
+//  Created by Alejandro Isaza on 2014-11-03.
+//  Copyright (c) 2014 Venture Media Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MusicKit.
+FOUNDATION_EXPORT double MusicKitVersionNumber;
+
+//! Project version string for MusicKit.
+FOUNDATION_EXPORT const unsigned char MusicKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MusicKit/PublicHeader.h>
+
+
