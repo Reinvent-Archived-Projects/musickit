b46e0afdb6fa06c816f7830c9fd96c4553c1cfc1
Alejandro Isaza <aisaza@venturemedia.com>
2014-11-12 12:13:43 -0800

    Add preview support for compressed files (fix #2)

---
+++ Podfile
@@ -0,0 +1,14 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+target :PreviewGenerator, exclusive: true do
+	platform :osx, "10.8"
+    pod "SSZipArchive"
+end
+
+target :MusicKitAppTests, exclusive: true do
+	platform :ios, "7.0"
+end
+
+target :MusicKitOSXAppTests, exclusive: true do
+	platform :osx, "10.8"
+end
--- PreviewGenerator/main.mm
+++ PreviewGenerator/main.mm
@@ -2,6 +2,7 @@
 
 #import <AppKit/AppKit.h>
 #import <MusicKit/MusicKit.h>
+#import <SSZipArchive/SSZipArchive.h>
 
 #include <mxml/geometry/ScoreGeometry.h>
 #include <mxml/geometry/PartGeometry.h>
@@ -17,6 +18,8 @@ static const CGFloat kScale = 2;
 
 
 int process(int argc, const char * argv[]);
+std::unique_ptr<mxml::dom::Score> loadMXL(NSString* filePath);
+std::unique_ptr<mxml::dom::Score> loadXML(NSString* filePath);
 bool renderScore(NSString* path, NSString* outputPrefix);
 
 
@@ -28,23 +31,32 @@ int main(int argc, const char * argv[]) {
 }
 
 void printUsage(int argc, const char * argv[]) {
-    std::cout << "Usage: \n";
-    std::cout << "    " << argv[0] << " <input> [<output>]\n\n";
-    std::cout << "    input   Input MusicXML file.\n";
-    std::cout << "    output  Optional output file. If not specified the input base name is used with a .png extension.\n";
+    std::cerr << "Usage: \n";
+    std::cerr << "    " << argv[0] << " <input> [<output>]\n\n";
+    std::cerr << "    input   Input MusicXML file. Either a raw .xml file or a compressed .mxl file.\n";
+    std::cerr << "    output  Optional output file. If not specified the input base name is used with a .png extension.\n";
 }
 
 int process(int argc, const char * argv[]) {
-    if (argc < 2) {
+    NSString* input = nil;
+    NSString* output = nil;
+    for (int i = 1; i < argc; i += 1) {
+        NSString* argument = [NSString stringWithUTF8String:argv[i]];
+        if ([argument hasPrefix:@"-"])
+            continue;
+
+        if (!input)
+            input = argument;
+        else if (!output)
+            output = argument;
+    }
+
+    if (!input) {
         printUsage(argc, argv);
         return 1;
     }
 
-    NSString* input = [NSString stringWithUTF8String:argv[1]];
-    NSString* output;
-    if (argc >= 3)
-        output = [NSString stringWithUTF8String:argv[2]];
-    else
+    if (!output)
         output = [[input stringByDeletingPathExtension] stringByAppendingPathExtension:@"png"];
 
     if (!renderScore(input, output))
@@ -53,12 +65,72 @@ int process(int argc, const char * argv[]) {
     return 0;
 }
 
+std::unique_ptr<mxml::dom::Score> loadMXL(NSString* filePath) {
+    NSArray* cachePathArray = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+    NSString* cachePath = [cachePathArray firstObject];
+    NSString* filename = [[filePath lastPathComponent] stringByDeletingPathExtension];
+    NSString* destPath = [cachePath stringByAppendingPathComponent:filename];
+
+    NSError* error;
+    BOOL success = [SSZipArchive unzipFileAtPath:filePath
+                                   toDestination:destPath
+                                       overwrite:YES
+                                        password:nil
+                                           error:&error
+                                        delegate:nil];
+    if (error)
+        NSLog(@"Error unzipping: %@", error);
+    if (!success) {
+        NSLog(@"Failed to unzip %@", filePath);
+        return std::unique_ptr<mxml::dom::Score>();
+    }
+
+    NSFileManager* fileManager = [[NSFileManager alloc] init];
+    NSArray* paths = [fileManager contentsOfDirectoryAtPath:destPath error:NULL];
+    NSString* xmlFile = nil;
+    for (NSString* file in paths) {
+        if ([file hasSuffix:@".xml"]) {
+            xmlFile = file;
+            break;
+        }
+    }
+    if (xmlFile == nil) {
+        NSLog(@"Archive does not contain an xml file: %@", filePath);
+        return std::unique_ptr<mxml::dom::Score>();
+    }
+
+    try {
+        NSString* xmlPath = [destPath stringByAppendingPathComponent:xmlFile];
+        std::ifstream is([xmlPath UTF8String]);
+
+        mxml::ScoreHandler handler;
+        lxml::parse(is, [filename UTF8String], handler);
+        return handler.result();
+    } catch (mxml::dom::InvalidDataError& error) {
+        NSLog(@"Error loading score '%@': %s", filePath, error.what());
+        return std::unique_ptr<mxml::dom::Score>();
+    }
+}
+
+std::unique_ptr<mxml::dom::Score> loadXML(NSString* filePath) {
+    mxml::ScoreHandler handler;
+    std::ifstream is([filePath UTF8String]);
+    lxml::parse(is, [filePath UTF8String], handler);
+    return handler.result();
+}
+
 bool renderScore(NSString* path, NSString* output) {
     // Parse input file
-    mxml::ScoreHandler handler;
-    std::ifstream is([path UTF8String]);
-    lxml::parse(is, [path UTF8String], handler);
-    std::unique_ptr<mxml::dom::Score> score = handler.result();
+    std::unique_ptr<mxml::dom::Score> score;
+    if ([path hasSuffix:@".xml"]) {
+        score = loadXML(path);
+    } else if ([path hasSuffix:@".mxl"]) {
+        score = loadMXL(path);
+    } else {
+        std::cerr << "File extension not recognized, assuming compressed MusicXML (.mxl).\n";
+        score = loadMXL(path);
+    }
+
     if (!score || score->parts().empty() || score->parts().front()->measures().empty())
         return false;
 
